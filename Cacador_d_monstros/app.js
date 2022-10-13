08eae98f6d261dadc5b1a7678bd0ef9ba2f15379
Murilo Lavratti <lavratti.murilo@gmail.com>
2022-10-13 15:02:30 -0300

    Commitando o jogo
    Foi um jogo que eu aprendi com um curso de Vue.JS, implementei algumas coisas novas e dei uma repaginada no CSS e HTML.

---
+++ Cacador_d_monstros/app.js
@@ -0,0 +1,66 @@
+new Vue({
+    el: '#app',
+    data: {
+        running: false,
+        playerLife: 100,
+        playerMana: 100,
+        monsterLife: 100,
+        logs: []   
+    },
+    computed: {
+        hasResult(){
+            return this.playerLife ==0 || this.monsterLife ==0
+        }
+    },
+    methods: {
+        startGame() {
+            this.running = true
+            this.playerLife = 100
+            this.playerMana = 100
+            this.monsterLife = 100
+            this.logs = []
+        },
+        attack(especial) {
+            this.hurt('monsterLife', 5, 10, especial, 'O caçador', 'Rathian', 'player')
+            if(this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Rathian', 'O caçador', 'monster')
+            }
+        },
+        hurt(prop, min, max, especial, source, target, cls) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+        },
+        healAndHurt(){
+            this.heal(15, 20)
+            this.hurt('playerLife', 7, 12, false, 'Rathian', 'Jogador', 'monster')
+        },
+        heal(min, max) {
+            if(this.playerMana > 0){
+                this.playerMana = Math.min(this.playerMana - 10, 100)
+            }
+            if(this.playerMana === 0)
+            {
+                this.registerLog(`O caçador não tem mais energia para se recuperar!`, 'player')
+            }else{
+                const heal = this.getRandom(min, max)
+                this.playerLife = Math.min(this.playerLife + heal, 100)
+                this.registerLog(`O caçador recuperou um total de ${heal} de Vida.`, 'player')
+            }
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        registerLog(text, cls){
+            this.logs.unshift( {text, cls} )
+        }
+    },
+    watch: {
+        hasResult(value) {
+            if (value) this.running = false
+        }
+
+    },
+})
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cacador-de-monstros
  Um joguinho besta que eu usei para treinar.
+
+ Não tenho certeza se eu vou atualizar alguma coisa dele, até pq ele literalmente foi criado para treinar, não possui o intuito de ser divertido.
